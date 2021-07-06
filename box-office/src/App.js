b437cb2ca9eefa4da69fda44dd4e384546fac3df
deep <deepmaheshwari23@gmail.com>
2021-07-06 11:49:51 +0530

    added show page

--- box-office/src/App.js
+++ box-office/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Starred from './pages/Starred';
+import Show from './pages/Show';
 
 function App() {
   return (
@@ -13,6 +14,12 @@ function App() {
 
       <Route exact path="/starred">
         <Starred />
+      </Route>
+
+      <Route exact path="/show/:id">
+        <div>
+          <Show />
+        </div>
       </Route>
 
       <Route>
